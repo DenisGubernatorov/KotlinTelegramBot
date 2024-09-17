3ea809c5bebb6902bca981b89c6262bcf27c9ebd
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-17 22:57:57 +0300

    KTB-01-create-wordsfile 1) Создание файла с парами слов 2) Вывод содержимого файла, для проверки работоспособности

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,11 @@
 package org.example
 
+import java.io.File
+
 fun main() {
+    val wordsFile = File("words.txt")
+    wordsFile.createNewFile()
+
+    val lines = wordsFile.readLines()
+    lines.forEach { println(it) }
 }
