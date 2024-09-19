a86d8faf56c6cf60fb841c989faefe995cc4a415
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-19 10:55:49 +0300

    KTB-04-string-parse 1) Добавлен data class Word, хранящий слово, его перевод и количество правильных ответов 2) Добавлен список dictionary, хранящий объекты типа Word 3) Получение значения для количества правильных ответов реализовано в отдельном методе getCorrectAnswersCountValue. Учитывается не только что значения может не быть, но и то, что может быть пустая строка 4) Заполнение dictionary. 5) Вывод в консоль элементов из dictionary

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,25 @@ package org.example
 import java.io.File
 
 fun main() {
+    val dictionary = mutableSetOf<Word>()
+
     val wordsFile = File("words.txt")
     wordsFile.createNewFile()
 
     val lines = wordsFile.readLines()
-    lines.forEach { println(it) }
+    lines.forEach {
+        val values = it.split("|")
+        val correctAnswersCount = getCorrectAnswersCountValue(values)
+
+        dictionary.add(Word(values[0], values[1], correctAnswersCount.toInt()))
+    }
+
+    dictionary.forEach {
+        println(it.toString())
+    }
+}
+
+private fun getCorrectAnswersCountValue(values: List<String>): String {
+    val tmpResult = values.getOrNull(2) ?: "0"
+    return if ((tmpResult).isBlank()) "0" else tmpResult
 }
