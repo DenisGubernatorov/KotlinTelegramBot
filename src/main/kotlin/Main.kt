7658bab111d8399f89e785a056b6f4fb1dd1a06d
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-09-26 16:16:56 +0300

    KTB-08-check-answer 1) wordsFile и dictionary сделаны свойствами класса, что бы было видно в других методах класса 2) Показ основного меню вынесен в отдельный методы, для возможности повторного использования
    3) Режим обучения
       3.1 В вариантах ответа добавлен пункт "Выход", возвращающий в основное меню
       3.2. Добавлена проверка на правильность введенного ответа. Выводы сообщений, в зависимости от правильности/ неправильности согласно ТЗ.
       3.3 При правильном ответе вызывается функция saveDictionary, обновляющая состояние файла со словами

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,10 +2,10 @@ package org.example
 
 import java.io.File
 
-fun main() {
-    val dictionary = mutableSetOf<Word>()
+private val wordsFile = File("words.txt")
+private val dictionary = mutableSetOf<Word>()
 
-    val wordsFile = File("words.txt")
+fun main() {
     wordsFile.createNewFile()
 
     val lines = wordsFile.readLines()
@@ -16,24 +16,25 @@ fun main() {
         dictionary.add(Word(values[0], values[1], correctAnswersCount))
     }
 
-    println(
-        """
-        Меню:
-        1 - Учить слова
-        2 - Статистика
-        3 - Выход
-        """.trimIndent(),
-    )
+    showMainMenu()
 
     while (true) {
-        when (val selectedMode = readln()) {
-            "1" -> println(selectedMode)
+        val learnedWords = dictionary.filter { it.correctAnswersCount >= 3 }.toSet()
+
+        when (readln()) {
+            "1" -> {
+                val wordsToLearn = dictionary - learnedWords
+                startLearning(wordsToLearn, learnedWords)
+                println("Вы вышли из режима обучения. Выберите следующее действие")
+                showMainMenu()
+            }
 
             "2" -> {
-                val learnedWords = dictionary.filter { it.correctAnswersCount >= 3 }
                 println(
                     "Выучено ${learnedWords.size} из ${dictionary.size} слов | ${(learnedWords.size / dictionary.size.toDouble() * 100).toInt()}%",
                 )
+                println("Выберите режим для продолжения")
+                showMainMenu()
             }
 
             "3" -> {
@@ -44,4 +45,72 @@ fun main() {
             else -> println("Неизвестный режим. Повторите ввод")
         }
     }
+}
+
+private fun showMainMenu() {
+    println(
+        """
+        Меню:
+        1 - Учить слова
+        2 - Статистика
+        3 - Выход
+        """.trimIndent(),
+    )
+}
+
+private fun startLearning(
+    wordsToLearn: Set<Word>,
+    learnedWords: Set<Word>,
+) {
+    if (wordsToLearn.isEmpty()) {
+        println("Вы выучили все слова")
+    } else {
+        wordsToLearn.forEach {
+            println("Слово: ${it.original}")
+            println("Варианты ответа:")
+            val variants =
+                when {
+                    wordsToLearn.size > 4 -> wordsToLearn.take(wordsToLearn.size).shuffled()
+                    else -> {
+                        val shuffled = wordsToLearn.shuffled()
+                        val take = learnedWords.shuffled().take(4 - shuffled.size)
+                        shuffled + take
+                    }
+                }
+            println("0 - Выход")
+            variants.forEachIndexed { index, word ->
+                println("${index + 1} - ${word.translate}")
+            }
+            print("Введите вариант ответа: ")
+            val answer = getCorrectAnswer()
+
+            if (answer == 0) {
+                return
+            } else if (answer - 1 !in wordsToLearn.indices || it != variants.elementAt(answer - 1)) {
+                println("Неправильно - ${it.original} [${it.translate}]")
+            } else {
+                println("Правильно!")
+                it.correctAnswersCount++
+                saveDictionary()
+            }
+            println()
+        }
+    }
+}
+
+private fun saveDictionary() {
+    val updated = dictionary.joinToString("\n") { "${it.original}|${it.translate}|${it.correctAnswersCount}" }
+
+    wordsFile.writeText(updated)
+}
+
+private fun getCorrectAnswer(): Int {
+    val answer = readln()
+
+    if (!answer.all { it.isDigit() } || answer.toInt() !in 0..4) {
+        println("Некорректный номер. Повторите ввод")
+        return getCorrectAnswer()
+    }
+
+    return answer.toInt()
 }
