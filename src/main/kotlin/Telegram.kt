c7d6fd7f3d166b3f69d879d8d4d1b2939b5cf84d
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-10-07 17:56:10 +0300

    KTB-11-tune-tg-bot 1) Адрес хоста вынесено в отдельную константу

--- src/main/kotlin/Telegram.kt
+++ src/main/kotlin/Telegram.kt
@@ -5,10 +5,13 @@ import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
+const val HOST_ADDRESS = "https://api.telegram.org"
+
 fun main(args: Array<String>) {
     val botToken = args[0]
-    val urlGetMe = "https://api.telegram.org/bot$botToken/getMe"
-    val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates"
+
+    val urlGetMe = "$HOST_ADDRESS/bot$botToken/getMe"
+    val urlGetUpdates = "$HOST_ADDRESS/bot$botToken/getUpdates"
 
     val client: HttpClient = HttpClient.newBuilder().build()
     val requestBuilder: HttpRequest.Builder = HttpRequest.newBuilder()
