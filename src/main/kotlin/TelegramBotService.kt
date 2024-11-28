3c6ccadf48b6285664bdbd336cfb4dc0fbd4dfa5
Denis Gubernatorov <dygubernatorov@gmail.com>
2024-11-29 00:23:30 +0300

    автодеплой тест 3

--- src/main/kotlin/TelegramBotService.kt
+++ src/main/kotlin/TelegramBotService.kt
@@ -61,7 +61,7 @@ class TelegramBotService(
         }
 
     fun getUpdates(updateId: Long): String {
-        //для теста автодеплоя 2
+        //для теста автодеплоя 3
         val urlGetUpdates = "$HOST_ADDRESS/bot$botToken/getUpdates?offset=$updateId"
         val updatesRequest: HttpRequest = HttpRequest.newBuilder().uri(URI.create(urlGetUpdates)).build()
 
