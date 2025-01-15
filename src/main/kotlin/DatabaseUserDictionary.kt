53d67bfe2c91d03a8fbce741d3ff652e6ef408dc
Denis Gubernatorov <dygubernatorov@gmail.com>
2025-01-16 00:32:51 +0300

    KTB-22-create-data-base 1) Доработка создания БД. Добавлены таблицы users и user_answers. 2) Добавлен интерфейс IUserDictionary и класс DatabaseUserDictionary реализующий IUserDictionary для работы через БД (пока тудушки)

---
+++ src/main/kotlin/DatabaseUserDictionary.kt
@@ -0,0 +1,27 @@
+package org.example
+
+class DatabaseUserDictionary : IUserDictionary {
+    override fun getNumOfLearnedWords(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getSize(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getLearnedWords(): List<Word> {
+        TODO("Not yet implemented")
+    }
+
+    override fun getUnlearnedWords(): List<Word> {
+        TODO("Not yet implemented")
+    }
+
+    override fun setCorrectAnswersCount(word: String, correctAnswersCount: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun resetUserProgress() {
+        TODO("Not yet implemented")
+    }
+}
--- src/main/kotlin/DictionaryDataSource.kt
+++ src/main/kotlin/DictionaryDataSource.kt
@@ -2,36 +2,56 @@ package org.example
 
 import java.io.File
 import java.sql.DriverManager
+import java.sql.Statement
 
 fun main() {
 
-
-    updateDictionary(File("words.txt"))
-}
-
-fun updateDictionary(file: File) {
-
-    val lines = file.readLines()
-
-    DriverManager.getConnection("jdbc:sqlite:data.db")
-        .use { connection ->
-            val statement = connection.createStatement()
-            statement.executeUpdate(
-                """
-                        CREATE TABLE IF NOT EXISTS 'words' (
+    DriverManager.getConnection("jdbc:sqlite:data.db").use { connection ->
+        val statement = connection.createStatement()
+        statement.executeUpdate(
+            """
+                    CREATE TABLE IF NOT EXISTS 'words' (
                     'id' integer PRIMARY KEY AUTOINCREMENT,
                     'text' varchar,
                     'translate' varchar
                 );
             """.trimIndent()
-            )
+        )
+        statement.executeUpdate(
+            """
+                CREATE TABLE IF NOT EXISTS 'users' (
+                  'id' integer PRIMARY KEY,
+                  'username' varchar,
+                  'created_at' timestamp,
+                  'chat_id' integer
+                );
+            """.trimIndent()
+        )
 
-            lines.forEach { it ->
-                val wordData = it.split("|")
-                statement.executeUpdate("insert into words (text, translate) values ('${wordData[0]}', '${wordData[1]}')")
-            }
+        statement.executeUpdate(
+            """
+                     CREATE TABLE IF NOT EXISTS  'user_answers' (
+                    'user_id' integer,
+                    'word_id' integer,
+                    'correct_answer_count' integer,
+                    'updated_at' timestamp,
+                    FOREIGN KEY ('word_id') REFERENCES 'words' ('id'),
+                    FOREIGN KEY ('user_id') REFERENCES 'users' ('id')
+                );
+            """.trimIndent()
+        )
+        updateDictionary(File("words.txt"), statement)
+    }
+}
 
+fun updateDictionary(file: File, statement: Statement) {
+    val lines = file.readLines()
 
-        }
+    lines.forEach {
+        val wordData = it.split("|")
+        statement.executeUpdate("insert into words (text, translate) values ('${wordData[0]}', '${wordData[1]}')")
+    }
 
 }
+
+
